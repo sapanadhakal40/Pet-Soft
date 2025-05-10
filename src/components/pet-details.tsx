92d6e465c999d1d2968cd6571b9be9ecf97e8e35
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 02:15:38 +0300

    Make client and add conditional rendering based on selection, create subcomponent for better organization

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -1,3 +1,77 @@
+"use client";
+
+import { usePetContext } from "@/lib/hooks";
+import { Pet } from "@/lib/types";
+import Image from "next/image";
+
 export default function PetDetails() {
-  return <section className="h-full w-full">Pet details</section>;
+  const { selectedPet } = usePetContext();
+  return (
+    <section className="h-full w-full flex flex-col">
+      {!selectedPet ? (
+        <EmptyView />
+      ) : (
+        <>
+          <TopBar pet={selectedPet} />
+          <OtherInfo pet={selectedPet} />
+          <Notes pet={selectedPet} />
+        </>
+      )}
+    </section>
+  );
+}
+
+type Props = {
+  pet: Pet | undefined;
+};
+
+function TopBar({ pet }: Props) {
+  return (
+    <div className="flex items-center bg-white px-8 py-5 boredr-b border-black[0.08]">
+      <Image
+        src={pet?.imageUrl || "/default-pet.jpg"}
+        alt="Selected pet image"
+        width={75}
+        height={75}
+        className="w-[75px] h-[75px] rounded-full object-cover"
+      />
+      <h2 className="text-3xl font-semibold leading-7 ml-5">{pet?.name}</h2>
+    </div>
+  );
+}
+
+function OtherInfo({ pet }: Props) {
+  return (
+    <div className="flex justify-around py-10 px-5 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+          Owner name
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.ownerName}</p>
+      </div>
+
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.age}</p>
+      </div>
+    </div>
+  );
+}
+
+function Notes({ pet }: Props) {
+  return (
+    <div>
+      <section className="bg-white flex-1 px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
+        {pet?.notes}
+      </section>
+    </div>
+  );
+}
+
+function EmptyView() {
+  return (
+    <p className="h-full flex justify-center items-center text-2xl font-medium">
+      No Pet selected
+    </p>
+  );
 }
