7bc295f8ce46856376a728f6732eb61329cd0a1c
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 01:28:51 +0300

    Add cn() utility function for active selected pet

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function PetList() {
-  const { pets, handleChangeSelectedPetId } = usePetContext();
+  const { pets, selectedPetId, handleChangeSelectedPetId } = usePetContext();
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
         <li key={pet.id}>
           <button
             onClick={() => handleChangeSelectedPetId(pet.id)}
-            className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-4 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+            className={cn(
+              "flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-4 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+              {
+                "bg-[#EFF1F2]": selectedPetId === pet.id,
+              }
+            )}
           >
             <Image
               src={pet.imageUrl}
