9b4acada92fba334be7ac11fe7b017556db38302
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 00:12:32 +0300

    Implement petlist data and render pet list data with mapping

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,28 @@
-export default function PetList() {
+import Image from "next/image";
+import { Pet } from "@/lib/types";
+
+type PetListProps = {
+  pets: Pet[];
+};
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button>Pet lists</button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-4 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet Image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
+
+// https://bytegrad.com/course-assets/projects/petsoft/api/pets
