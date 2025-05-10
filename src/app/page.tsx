98d2bcfc49fd41ad9fc642e0d8a94895e97c084b
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-10 16:29:56 +0300

    Add link to signup and login buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -22,8 +23,12 @@ export default function Home() {
           access for $290.
         </p>
         <div className="mt-10 space-x-4">
-          <Button>Get Started</Button>
-          <Button variant="secondary">Log In</Button>
+          <Button asChild>
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button asChild variant="secondary">
+            <Link href="/login">Log In</Link>
+          </Button>
         </div>
       </div>
     </main>
