230d231ec9e2fe518b6b04506c4565009552d269
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-10 15:50:10 +0300

    Add layout,text content and image to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10 ">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        width={519}
+        height={472}
+        alt="Preview of PetSoft"
+      />
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="font-medium text-2xl max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care.Get lifetime
+          access for $290.
+        </p>
+        <div className="mt-10">
+          <button>Sign up</button>
+        </div>
+      </div>
+    </main>
+  );
 }
