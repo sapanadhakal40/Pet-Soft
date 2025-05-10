6f690886ac52cf216f3fc906ff147a2def62d121
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 00:04:55 +0300

    Fetch petlist data in server component

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -5,7 +5,15 @@ import PetList from "@/components/pet-list";
 import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
-export default function page() {
+export default async function page() {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
@@ -20,7 +28,7 @@ export default function page() {
 
         <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
           <ContentBlock>
-            <PetList />
+            <PetList pets={data} />
           </ContentBlock>
         </div>
 
