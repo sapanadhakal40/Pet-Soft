347bcecf96d95593481f7dba59e7ba3dc8c155b6
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-10 17:35:24 +0300

    Add footer content and styling

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>AF</div>;
+  return (
+    <footer className="border-t border-black/10 py-5 mt-auto ">
+      <small className="opacity-50 ">
+        &copy; 2025 PetSoft. All rights reserved.
+      </small>
+    </footer>
+  );
 }
