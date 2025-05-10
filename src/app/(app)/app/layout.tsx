22e9ac9b2119aedfdf71c9aa16003cc0350c0c44
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 01:03:48 +0300

    Move data fetching logic to layout

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,14 +1,27 @@
 import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
+import PetContextProvider from "@/context/pet-context-provider";
+import { Pet } from "@/lib/types";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data: Pet[] = await response.json();
   return (
     <>
       <BackgroundPattern />
       <div className=" flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>{children}</PetContextProvider>
         <AppFooter />
       </div>
     </>
