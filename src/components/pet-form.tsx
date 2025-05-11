f8826b288991c60b831a7469e349fac52924e4b3
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 14:08:46 +0300

    add form validation, submission handling, reset form logic onFormSubmission call and add type

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePetContext } from "@/lib/hooks";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
@@ -5,30 +7,58 @@ import { Textarea } from "./ui/textarea";
 
 type PetFormProps = {
   actionType: "add" | "edit";
+  onFormSubmission: () => void;
 };
-export default function PetForm({ actionType }: PetFormProps) {
+export default function PetForm({
+  actionType,
+  onFormSubmission,
+}: PetFormProps) {
+  const { handleAddPet } = usePetContext();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const newPet = {
+      name: formData.get("name") as string,
+      ownerName: formData.get("ownerName") as string,
+      imageUrl:
+        (formData.get("imageUrl") as string) ||
+        "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+      age: +(formData.get("age") as string),
+      notes: formData.get("notes") as string,
+    };
+    handleAddPet(newPet);
+    // console.log(newPet);
+    onFormSubmission();
+  };
   return (
-    <form className="flex flex-col">
+    <form onSubmit={handleSubmit} className="flex flex-col">
       <div className="space-y-4">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
-          <Input id="name" type="text" />
+          <Input id="name" type="text" name="name" required />
         </div>
         <div className="space-y-1">
           <Label htmlFor="ownerName">Owner Name</Label>
-          <Input id="ownerName" type="text" />
+          <Input id="ownerName" type="text" name="ownerName" required />
         </div>
         <div className="space-y-1">
           <Label htmlFor="imageUrl">Image Url</Label>
-          <Input id="imageUrl" type="text" />
+          <Input id="imageUrl" type="text" name="imageUrl" />
         </div>
         <div className="space-y-1">
           <Label htmlFor="age">Age</Label>
-          <Input id="age" type="number" />
+          <Input id="age" type="number" name="age" required />
         </div>
         <div className="space-y-1">
           <Label htmlFor="notes">Notes</Label>
-          <Textarea id="notes" rows={3} placeholder="Notes" />
+          <Textarea
+            id="notes"
+            rows={3}
+            placeholder="Notes"
+            name="notes"
+            required
+          />
         </div>
       </div>
       <Button type="submit" className="mt-5 self-end">
