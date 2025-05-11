479905733fb4b57d2702c0222a8b71df1bc592e5
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 19:13:18 +0300

    Implemement petcheckout using server action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -47,3 +47,18 @@ export async function editPet(petId, formData) {
   }
   revalidatePath("/app", "layout");
 }
+
+export async function deletePet(petId) {
+  try {
+    await prisma.pet.delete({
+      where: {
+        id: petId,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Error deleting pet",
+    };
+  }
+  revalidatePath("/app", "layout");
+}
