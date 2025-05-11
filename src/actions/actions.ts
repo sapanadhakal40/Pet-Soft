c8e40e35c48287aed269a38a06d597e0bc09fdc9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-11 17:17:25 +0300

    Create new pets in database using server action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,8 +1,17 @@
 "use server";
 import prisma from "@/lib/db";
+export async function addPet(formData) {
+  const DEFAULT_IMAGE =
+    "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png";
 
-export async function addPet(pet) {
+  console.log(formData);
   await prisma.pet.create({
-    data: pet,
+    data: {
+      name: formData.get("name"),
+      ownerName: formData.get("ownerName"),
+      imageUrl: formData.get("imageUrl") || DEFAULT_IMAGE,
+      age: parseInt(formData.get("age")),
+      notes: formData.get("notes"),
+    },
   });
 }
