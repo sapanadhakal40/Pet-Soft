b94ec63f114077580290cc173a6065f558276980
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 17:06:02 +0300

    Add authorization callback for route, password comparison with bcrypt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,59 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const config = {
   pages: {
     signIn: "/login",
   },
-  providers: [],
+
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        //runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+        if (!passwordMatch) {
+          console.log("Invalid credentials");
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
   callbacks: {
-    authorized: ({ request }) => {
+    authorized: ({ auth, request }) => {
+      // runs on every request with middleware
+      const isLoggedIn = Boolean(auth?.user);
       const isTryingToaccessApp = request.nextUrl.pathname.includes("/app");
 
-      if (isTryingToaccessApp) {
+      if (!isLoggedIn && isTryingToaccessApp) {
         return false;
-      } else {
+      }
+      if (isLoggedIn && isTryingToaccessApp) {
+        return true;
+      }
+      if (!isTryingToaccessApp) {
         return true;
       }
     },
   },
 } satisfies NextAuthConfig;
 
-export const { auth } = NextAuth(config);
+export const { auth, signIn } = NextAuth(config);
