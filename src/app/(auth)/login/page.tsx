46c0dc1ee5462e38589b6d49c282ff837ec9d6c4
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 09:20:33 +0300

    Add login and signup pages with authentication forms, add naviagtion links between pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,24 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Login Page</div>;
+  return (
+    <main className="max-w-md mx-auto ">
+      <div className=" p-10 rounded-lg border border-zinc-200 shadow-md">
+        <H1 className="text-center mb-4">Log In</H1>
+        <AuthForm type="login" />
+
+        <p className="mt-4 text-sm text-zinc-500">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/signup"
+            className=" text-blue-500 font-medium hover:underline"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,24 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Sign up page</div>;
+  return (
+    <main className="max-w-md mx-auto ">
+      <div className=" p-8 rounded-lg border border-zinc-200 shadow-md">
+        <H1 className="text-center mb-4">Sign Up</H1>
+        <AuthForm type="signup" />
+
+        <p className="mt-4 text-sm text-zinc-500">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className=" font-medium text-blue-500 hover:underline"
+          >
+            Log in
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 }
