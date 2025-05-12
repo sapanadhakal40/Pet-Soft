1a08d522fd17e78bac623774a856f1c58c9399a2
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 18:53:04 +0300

    Add sign out button in page.tsx

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,6 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import SignOutBtn from "@/components/sign-out-btn";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -12,8 +13,10 @@ export default async function page() {
     <main className="">
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="h-[500px] flex justify-center items-center">
+      <ContentBlock className="h-[500px] flex flex-col justify-center items-center gap-3">
         <p>Logged in as {session?.user.email}</p>
+
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
