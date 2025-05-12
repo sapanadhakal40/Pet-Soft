5293d2b0b97ba8428803efe7a11eb21ac4e29fc6
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 20:42:48 +0300

    Add signup action with password hashing and redirect adter authentication

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,13 +2,30 @@
 import { signIn, signOut } from "@/lib/auth";
 import prisma from "@/lib/db";
 import { petFormSchema, petIdSchema } from "@/lib/validations";
-// import { sleep } from "@/lib/utils";
+import bcrypt from "bcryptjs";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+// import { sleep } from "@/lib/utils";
 
 //-----User actions-----//
+
 export async function logIn(formData: FormData) {
-  const authData = Object.fromEntries(formData.entries());
-  await signIn("credentials", authData);
+  await signIn("credentials", formData);
+  redirect("/app/dashboard");
+}
+export async function signUp(formData: FormData) {
+  const hashedPassword = await bcrypt.hash(
+    formData.get("password") as string,
+    10
+  );
+
+  await prisma.user.create({
+    data: {
+      email: formData.get("email") as string,
+      hashedPassword,
+    },
+  });
+  await signIn("credentials", formData);
 }
 
 export async function logOut() {
