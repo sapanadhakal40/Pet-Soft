0801c36584819ffd3b247ea5d52d42a50343cd71
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 18:09:15 +0300

    Add user  login session to display in UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,13 +1,19 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
   return (
     <main className="">
       <H1 className="my-8 text-white">Your Account</H1>
 
       <ContentBlock className="h-[500px] flex justify-center items-center">
-        <p>Logged in as ...</p>
+        <p>Logged in as {session?.user.email}</p>
       </ContentBlock>
     </main>
   );
