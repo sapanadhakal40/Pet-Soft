02889b3a5cfb74f83790379d0666847183393866
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-12 22:33:45 +0300

    Import server only and create database query function

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,5 +1,9 @@
+import "server-only";
+import prisma from "@/lib/db";
+
 import { redirect } from "next/navigation";
 import { auth } from "./auth";
+import { Pet, User } from "@prisma/client";
 
 export async function checkAuth() {
   const session = await auth();
@@ -7,4 +11,30 @@ export async function checkAuth() {
     redirect("/login");
   }
   return session;
+}
+
+export async function getUserByEmail(email: User["email"]) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+}
+export async function getPetById(petId: Pet["id"]) {
+  const pet = await prisma.pet.findUnique({
+    where: {
+      id: petId,
+    },
+  });
+  return pet;
+}
+
+export async function getPetsByUserId(userId: User["id"]) {
+  const pets = await prisma.pet.findMany({
+    where: {
+      userId,
+    },
+  });
+  return pets;
 }
