579bd7458824974d06bb3426e86c47bc906aa4cd
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 00:20:00 +0300

    Implement validation for signup reusing the authschema

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -12,7 +12,7 @@ import { getPetById } from "@/lib/server-utils";
 //-----User actions-----//
 
 export async function logIn(formData: unknown) {
-  if (!formData) {
+  if (!(formData instanceof FormData)) {
     return {
       message: "Invalid form data",
     };
@@ -21,15 +21,30 @@ export async function logIn(formData: unknown) {
   redirect("/app/dashboard");
 }
 
-export async function signUp(formData: FormData) {
-  const hashedPassword = await bcrypt.hash(
-    formData.get("password") as string,
-    10
-  );
+export async function signUp(formData: unknown) {
+  //check if formData is formData type
+  if (!(formData instanceof FormData)) {
+    return {
+      message: "Invalid form data",
+    };
+  }
+  //convert formdata to object
+  const formDataEntries = Object.fromEntries(formData.entries());
+
+  //validation
+  const validatedFormData = authFormSchema.safeParse(formDataEntries);
+  if (!validatedFormData.success) {
+    return {
+      message: "Invalid form data",
+    };
+  }
+  const { email, password } = validatedFormData.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   await prisma.user.create({
     data: {
-      email: formData.get("email") as string,
+      email,
       hashedPassword,
     },
   });
