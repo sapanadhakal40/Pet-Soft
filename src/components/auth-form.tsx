669220f9809d0552b84213a063a5ad62c88e86b9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 01:09:50 +0300

    Add useFormState hook for login and signup error handling and add authformbtn component

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -1,22 +1,28 @@
+"use client";
+
 import { logIn, signUp } from "@/actions/actions";
-import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { useFormState, useFormStatus } from "react-dom";
+import AuthFormBtn from "./auth-form-btn";
 
 type AuthFormProps = {
   type: "logIn" | "signUp";
 };
 export default function AuthForm({ type }: AuthFormProps) {
+  const [signUpError, dispatchSignUp] = useFormState(signUp, undefined);
+  const [logInError, dispatchLogIn] = useFormState(logIn, undefined);
+
   return (
-    <form action={type === "logIn" ? logIn : signUp}>
-      <div className="space-y-1">
-        <Label htmlFor="email" className="text-sm font-medium ">
+    <form action={type === "logIn" ? dispatchLogIn : dispatchSignUp}>
+      <div className="space-y-2">
+        <Label htmlFor="email" className="text-base font-medium ">
           Email
         </Label>
         <Input id="email" name="email" type="email" required maxLength={100} />
       </div>
       <div className="mt-2 mb-4 space-y-1">
-        <Label htmlFor="password" className="text-sm font-medium">
+        <Label htmlFor="password" className="text-base font-medium">
           Password
         </Label>
         <Input
@@ -27,9 +33,15 @@ export default function AuthForm({ type }: AuthFormProps) {
           maxLength={100}
         />
       </div>
-      <Button className="mt-4">
-        {type === "logIn" ? "Log In" : "Sign Up"}
-      </Button>
+
+      <AuthFormBtn type={type} />
+
+      {signUpError && (
+        <p className="text-red-500 text-sm mt-2">{signUpError.message}</p>
+      )}
+      {logInError && (
+        <p className="text-red-500 text-sm mt-2">{logInError.message}</p>
+      )}
     </form>
   );
 }
