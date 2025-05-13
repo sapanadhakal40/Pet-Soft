71f45c74f7ce8e4b4feda95e5555ef110f37ffc9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 18:14:49 +0300

    Add webhook endpoint for payment processing and handle post request for stripes webhook events, update hasAccess in database

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/lib/db";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  //   console.log("body", body);
+
+  //verify webhook came from stripe
+
+  //fullfill the request
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  });
+
+  return Response.json(null, {
+    status: 200,
+  });
+}
