603abbddaf56d06345c5e33dafa20f9b39330df3
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 20:20:08 +0300

    Add loading statewhile updating

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -13,7 +13,7 @@ export default function Page({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const [isPending, startTransition] = useTransition();
-  const { update } = useSession();
+  const { data: session, update, status } = useSession();
   const router = useRouter();
   return (
     <main className="flex flex-col items-center space-y-10">
@@ -26,6 +26,7 @@ export default function Page({
             //redirect to dashboard
             router.push("/app/dashboard");
           }}
+          disabled={status === "loading" || session?.user.hasAccess}
         >
           Access Petsoft
         </Button>
