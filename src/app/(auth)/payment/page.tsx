1000535e2e1de800884ed1c2a064dc2cbe8602d9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 08:51:35 +0300

    Add success and canceled search params to URL after stripe checkout

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -4,17 +4,30 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 
-export default function page() {
+export default function page({ searchParams }) {
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for $299
-      </Button>
+
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession();
+          }}
+        >
+          Buy lifetime access for $299
+        </Button>
+      )}
+      {searchParams?.success && (
+        <p className="text-md text-green-500">
+          Payment Successful.You can now access the app.
+        </p>
+      )}
+      {searchParams?.canceled && (
+        <p className="text-md text-red-500">
+          Payment canceled. Please try again.
+        </p>
+      )}
     </main>
   );
 }
