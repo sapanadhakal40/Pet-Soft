739d3f75b22f9a509bb115450ec5f592b61776f0
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 18:19:22 +0300

    update nextauth types for user and JWT

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -3,12 +3,12 @@ import { User } from "next-auth";
 declare module "next-auth" {
   interface User {
     hasAccess: boolean;
+    email: string;
   }
 
   interface Session {
     user: User & {
       id: string;
-      hasAccess: boolean;
     };
   }
 }
@@ -16,6 +16,7 @@ declare module "next-auth" {
 declare module "@auth/core/jwt" {
   interface JWT {
     userId: string;
+    email: string;
     hasAccess: boolean;
   }
 }
