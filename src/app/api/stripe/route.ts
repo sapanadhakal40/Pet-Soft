3653c6f1953b64e20932b99d621c734a8935985f
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-13 20:26:42 +0300

    Implement webhook verification, error handling and switch case for handling diffrent stripes event

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,20 +1,43 @@
 import prisma from "@/lib/db";
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature");
   //   console.log("body", body);
 
   //verify webhook came from stripe
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    console.log("Error verifying stripe webhook", err);
+    return Response.json(null, {
+      status: 400,
+    });
+  }
 
   //fullfill the request
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      break;
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
 
   return Response.json(null, {
     status: 200,
